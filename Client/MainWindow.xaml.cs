3eb44e89aeccc3e07cb4d289f1ba2cead5e7af2a
true_mogician <925269713@qq.com>
2021-12-16 17:26:21 +0800

    feat: main window

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace Client {
+	/// <summary>
+	///     Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public const string UserCredentialDirectory = "Users";
+
+		public MainWindow() {
+			InitializeComponent();
+			if (!Directory.Exists(UserCredentialDirectory))
+				Directory.CreateDirectory(UserCredentialDirectory);
+		}
+
+		private void CreateUserButtonClick(object sender, RoutedEventArgs args) { }
+	}
+}
