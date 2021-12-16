c6964cada70b3e979e1287bad7c045f4119594a5
true_mogician <925269713@qq.com>
2021-12-16 17:27:41 +0800

    feat: login dialog

---
+++ Client/LoginDialog.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Client {
+	/// <summary>
+	///     Interaction logic for LoginDialog.xaml
+	/// </summary>
+	public partial class LoginDialog : Window {
+		private bool _canceled;
+
+		public LoginDialog() {
+			InitializeComponent();
+		}
+
+		public new static (string Username, string Password)? ShowDialog() {
+			var dialog = new LoginDialog();
+			(dialog as Window).ShowDialog();
+			return dialog._canceled ? null : (dialog.UsernameTextBox.Text, dialog.PasswordTextBox.Password);
+		}
+
+		public new static (string Username, string Password)? Show() {
+			var dialog = new LoginDialog();
+			(dialog as Window).Show();
+			return dialog._canceled ? null : (dialog.UsernameTextBox.Text, dialog.PasswordTextBox.Password);
+		}
+
+		private void TextBoxKeyUp(object sender, KeyEventArgs args) {
+			if (args.Key != Key.Enter)
+				return;
+			if (sender.Equals(UsernameTextBox))
+				PasswordTextBox.Focus();
+			else
+				ConfirmButtonClick(sender, args);
+		}
+
+		private void ConfirmButtonClick(object sender, RoutedEventArgs args) {
+			Close();
+		}
+
+		private void CancelButtonClick(object sender, RoutedEventArgs args) {
+			_canceled = true;
+			Close();
+		}
+	}
+}
